839d50bf0c403b2a3204e1aee43bbff3d9ed42a8
Eky Umroh.com <ekyzulianto@umroh.com>
2020-04-22 23:08:04 +0700

    add middleware and store

---
+++ store/index.js
@@ -0,0 +1,37 @@
+export const state = () => ({ 
+    auth: false,
+    
+})
+
+// export const mutations = { 
+//     SET_POPUP_DRAFT(state, show){
+//         state.popupDraft = show
+//     },
+//     SET_POPUP_SAVE_DRAFT(state, show){
+//         state.popupToSaveDraft = show
+//     },
+//     SET_DATA_ID_PACKAGE(state, id) {
+//         state.dataIdPackage = id
+//     },
+//     SET_DELETABLE(state, deletable) {
+//         state.deletable = deletable
+//     }
+// }
+
+// export const actions = { 
+//     setPopupDraftShow: ({commit}, show) => {
+//         commit('SET_POPUP_DRAFT', show)
+//     },
+//     setPopupSaveDraftShow: ({commit}, show) => {
+//         commit('SET_POPUP_SAVE_DRAFT', show)
+//     },
+//     setDataIdPack: ({commit}, id) => {
+//         commit('SET_DATA_ID_PACKAGE', id)
+//     }
+// }
+
+// export const getters = {
+//     getDetetable: state => {
+//         return state.deletable
+//     }
+// }
