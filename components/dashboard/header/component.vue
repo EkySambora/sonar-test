e213a222fd4c930f93709ea764cffaed6397e32a
Eky Umroh.com <ekyzulianto@umroh.com>
2020-04-24 02:53:08 +0700

    add method logout

--- components/dashboard/header/component.vue
+++ components/dashboard/header/component.vue
@@ -11,10 +11,20 @@
                 <span></span>
                 <span></span>
             </div>
-            <div class="logout-btn">
+            <div class="logout-btn" @click="logout">
                 Logout
             </div>
         </div>
     </div>
 </template>
+<script>
+export default {
+    methods:{
+        logout(){
+            this.$store.dispatch('setAuth', false)
+            this.$nuxt.$router.push({path : '/login'})
+        }
+    }
+}
+</script>
 <style lang="scss" src="./style.scss" />
